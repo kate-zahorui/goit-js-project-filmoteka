e3c0bdb70b920982670a080bd710746dcedfd3ba
Oleksandr26 <alemaevaleksandr96@gmail.com>
2022-07-22 18:29:30 +0300

    markup-films-card

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import debounce from 'lodash.debounce';
 import Delivery from './js/Delivery';
 import getGenres from './js/getGenre';
+import { createMarkup } from './js/markupFilmCard';
 
+const listRef = document.querySelector('.list__film');
 const search = document.querySelector('#search-box');
 const delivery = new Delivery();
 
@@ -19,6 +21,8 @@ async function getMovies() {
   let data;
   try {
     data = delivery.query ? await delivery.search() : await delivery.trend();
+    const markup = createMarkup(data.results);
+    listRef.innerHTML = markup;
   } catch (error) {
     console.log('ERROR = ', error);
   }
