895a3e71596ff96a3081152d67a29788ae3b8eb0
kate-zahorui <kate.zahorui@gmail.com>
2022-07-24 09:39:21 +0300

    Adds dynamic modal content
    Adds searching and adding content of the selected movie into the modal.

--- src/js/Delivery.js
+++ src/js/Delivery.js
@@ -33,6 +33,11 @@ class Delivery {
     this.page += 1;
     return response.data;
   }
+
+  async fetchById(movieId) {
+    const response = await axios.get(`movie/${movieId}?api_key=${API_KEY}`);
+    return response.data;
+  }
 }
 
 export default Delivery;
