414c03c11fffa42b716a30392722192efe65b431
kate-zahorui <kate.zahorui@gmail.com>
2022-07-26 16:52:43 +0300

    Adds movie trailer

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,9 +4,11 @@ import { modalLibraryMarkup } from './modalLibraryMarkup';
 import './buttonsModal';
 import { addModalButtons, getMovie } from './buttonsModal';
 import svg from '../images/icons.svg';
+import { Notify } from 'notiflix';
 
 const delivery = new Delivery();
 let modalRef = {};
+let movieId = 0;
 
 const listRef = document.querySelector('.list__film');
 
@@ -18,12 +20,13 @@ async function onFilmClick(e) {
         return;
     }
     const movieCard = e.target.closest('li[id]');
-    const movieId = getId(movieCard);
+    movieId = getId(movieCard);
     const movie = await getMovieById(movieId);
 
     openModal(movie);
     getMovie(movie);
     addModalButtons();
+    addTrailerPlayButton();
 }
 
 function getId(movieCard) {
@@ -80,4 +83,58 @@ function modalCloseByEsc(event) {
     if (isEscKey) {
         modalRef.close();
     }
+}
+
+// Trailer -----------------------------------------------------------------------
+
+function addTrailerPlayButton() {
+    const btnPlayRef = document.querySelector('.js-playtrailer');
+    btnPlayRef.addEventListener('click', onTrailerPlay);
+}
+
+async function onTrailerPlay(event) {
+    event.preventDefault();
+    const movieVideos = await getTrailerById(movieId);
+    console.log(movieVideos);
+    const movieVideosall = movieVideos.results;
+    const movieTrailer = movieVideosall.find(
+        video => video.type === 'Trailer' && video.site === 'YouTube'
+    );
+    if (movieTrailer) {
+        openTrailerModal(movieTrailer);
+    } else {
+        Notify.failure('Sorry, we can`t find any trailer of this movie.');
+    }
+    console.log(movieTrailer);
+}
+
+async function getTrailerById(movieId) {
+    try {
+        const videos = await delivery.fetchTrailer(movieId);
+        return videos;
+    } catch (error) {
+        console.log('ERROR = ', error);
+    }
+    console.log(data);
+}
+
+function openTrailerModal(movieTrailer) {
+    const instance = basicLightbox.create(
+        `
+  <iframe width="560" height="315" 
+  src="https://www.youtube.com/embed/${movieTrailer.key}" 
+  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen></iframe>
+`,
+        {
+            onShow: instance => {
+                instance.element();
+                document.body.setAttribute('style', 'overflow: hidden');
+            },
+            onClose: () => {
+                document.body.removeAttribute('style');
+            },
+        }
+    );
+    instance.show();
 }
